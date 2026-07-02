223c47a1f7c242d2943e68c3e560279d5bb64da6
Rithika <t.rithika02@gmail.com>
2026-07-02 11:03:21 +0530

    First commit

---
+++ src/test/java/Base_Programs1/TC01_EDU.java
@@ -0,0 +1,38 @@
+package Base_Programs1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC01_EDU {
+
+WebDriver driver;
+	
+	@Test(groups= {"smoke"})
+	public void TEST() {
+	
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");	
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		driver.findElement(By.xpath("//span[text()='Admin']")).click();
+		driver.findElement(By.xpath("//span[text()='Qualifications ']")).click();
+		driver.findElement(By.xpath("//a[text()='Education']")).click();
+		driver.findElement(By.xpath("//button[text()=' Add ']")).click();
+		
+		driver.findElement(By.xpath("(//input[@class='oxd-input oxd-input--active'])[2]")).sendKeys("ALPHA");
+		driver.findElement(By.xpath("//button[text()=' Save ']")).click();
+
+	  driver.quit();
+	
+	
+}
+}
---
+++ src/test/java/Base_Programs1/TC01_NATION.java
@@ -0,0 +1,37 @@
+package Base_Programs1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC01_NATION {
+	
+	WebDriver driver;
+	
+	@Test(groups= {"regression"})
+	public void TEST() {
+	
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");	
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		driver.findElement(By.xpath("//span[text()='Admin']")).click();
+		driver.findElement(By.xpath("//a[text()='Nationalities']")).click();
+		driver.findElement(By.xpath("//button[text()=' Add ']")).click();
+		
+		driver.findElement(By.xpath("(//input[@class='oxd-input oxd-input--active'])[2]")).sendKeys("alpha12");
+		driver.findElement(By.xpath("//button[text()=' Save ']")).click();
+		
+		  driver.quit();
+		
+	}
+
+}
